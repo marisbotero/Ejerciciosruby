bdb43a547ab952286aedbe600ace553849bdd50f
Maris Botero <maris2143@gmail.com>
2016-02-15 14:30:32 -0500

    Promedio de las notas de un alumno

---
+++ promedio.rb
@@ -0,0 +1,13 @@
+ puts "escriba la nota1"
+ nota1=gets.chomp
+
+ puts "escriba la nota2"
+ nota2=gets.chomp
+
+ puts "escriba la nota3"
+ nota3=gets.chomp
+
+
+ sumanota=(nota1.to_i+nota2.to_i+nota3.to_i)/ 3
+
+puts sumanota
